6aacbdcfc82739c83be0ba9c6e7d5f2b208fbc28
FalseLuc033 <lguerra3333@gmail.com>
2023-06-28 13:29:38 -0300

    input radio habite-se 80 habilitado

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,14 @@ const form = document.getElementById("form");
 const cadastro = document.getElementById("inputCadastro");
 const interessado = document.getElementById("inputInteressado");
 const profissional = document.getElementById("inputProfissional");
-let radioTxHabitese = document.getElementsByName("radioTxHabitese");
+const radioTxHabitese = document.getElementsByName("radioTxHabitese");
 //const assEng = document.getElementsByName("radioAssinatura");
 
 
+const txHabitese80 = 111.88;
 let txHabiteseSelected = 0;
+
+
 let assEngSelected = 0;
 
 //Verificando qual dos radio do campo de Tx de Habite-se está selecionado e atribuindo seu respectivo valor
@@ -19,19 +22,43 @@ function selectTxHabitese() {
         if (radioTxHabitese[i].checked) {
             console.log('\nEntrou no radio checked')
             if (radioTxHabitese[i].value === '1') {
-                console.log('\nAtribuiu o valor da tx selecionada e validou como trua a radio index '+radioTxHabitese[i].value)
-                radioTxHabitese[i].checked = true;
-                txHabiteseSelected = 111.88;
+                txHabiteseSelected = txHabitese80;
+                console.log('Habite-se de até 80 m² radio ('+radioTxHabitese[i].value + ')');
             }else{
                 console.log('\nErro no value')
             }
         }
     }
-    console.log('value do radioTxHabitese: ' + radioTxHabitese +'|| txhabiteseSelected: ' + txHabiteseSelected);
+    console.log('Valor do ' + txhabiteseSelected + txHabiteseSelected);
 }
 
-function teste(){
-    console.log('teste funcao');
+function selectTxHabitese2() {
+    for (let i = 0; i < radioTxHabitese.length; i++) {
+        if (radioTxHabitese[i].checked) {
+            switch (radioTxHabitese[i].value) {
+                case '1':
+                    txHabiteseSelected = 111.88;
+                    console.log('value do radioTxHabitese: '+radioTxHabitese+'|| txhabiteseSelected: '+txHabiteseSelected);
+                    break;            
+                case '2':
+                    radioTxHabitese[i].checked = true;
+                    txHabiteseSelected = 167.87;
+                    break;            
+                case '3':
+                    radioTxHabitese[i].checked = true;
+                    txHabiteseSelected = 251.78;
+                    break;            
+                case '4':
+                    radioTxHabitese[i].checked = true;
+                    txHabiteseSelected = 378.41;
+                    break;
+                default:
+                console.log("erro na validação do radio tx Habite-se");
+            }
+            console.log('value do radioTxHabitese: '+radioTxHabitese+'|| txhabiteseSelected: '+txHabiteseSelected);
+        }
+
+    }
 }
 
 form.addEventListener('submit', (event) => {
@@ -67,31 +94,44 @@ form.addEventListener('submit', (event) => {
    form.submit();
 });
 
-function selectTxHabitese2() {
+
+/*
+@ -1,36 +0,0 @@
+// DOM 
+const form = document.getElementById('myForm');
+const cadastro = document.getElementById('input-cadastro');
+const radioTxHabitese = document.getElementsByName('radio-tx-habitese');
+const radioAssinatura = document.getElementsByName('radio-assinatura');
+
+let entradaHabitese = 0;
+
+// Variáveis das taxas fixas
+const txHabitese80 = 147.45;
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    retornaValorRadioHabitese();
+})
+
+function retornaValorRadioHabitese() {
     for (let i = 0; i < radioTxHabitese.length; i++) {
         if (radioTxHabitese[i].checked) {
-            switch (radioTxHabitese[i].value) {
-                case '1':
-                    txHabiteseSelected = 111.88;
-                    console.log('value do radioTxHabitese: '+radioTxHabitese+'|| txhabiteseSelected: '+txHabiteseSelected);
-                    break;            
-                case '2':
-                    radioTxHabitese[i].checked = true;
-                    txHabiteseSelected = 167.87;
-                    break;            
-                case '3':
-                    radioTxHabitese[i].checked = true;
-                    txHabiteseSelected = 251.78;
-                    break;            
-                case '4':
-                    radioTxHabitese[i].checked = true;
-                    txHabiteseSelected = 378.41;
-                    break;
-                default:
-                console.log("erro na validação do radio tx Habite-se");
+            if (radioTxHabitese[i].value == '1'){
+                entradaHabitese = txHabitese80;
+                console.log("funcionou");
             }
-            console.log('value do radioTxHabitese: '+radioTxHabitese+'|| txhabiteseSelected: '+txHabiteseSelected);
+            
+            alert(
+                "radio (" + radioTxHabitese[i].value + ") checked" +
+                "\n" +
+                "Valor retornado: " +
+                valorRetornado +
+                "\n valor de a: "+ a
+                );
+            
+            }
         }
-
     }
-}
+    
+
+*/
